e7071bf595356b2da510a23dd55b018e2cd27874
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2020-06-01 00:26:41 -0700

    Implement Login, Search, Commit.

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,140 @@
 import * as vscode from 'vscode';
+import { MWClient, TokenType } from './mwclient';
+import path = require('path');
+import { existsSync, mkdirSync } from 'fs';
+
+let articlePicker = vscode.window.createQuickPick();
+let SAVE_ROOT: vscode.Uri;
+
+let debounceTimeoutId: NodeJS.Timeout;
+const DEBOUNCE_THRESHOLD = 200;
+
+function articlePickerDidChangeValueHandler(val: string) {
+	if (!val) {
+		return;
+	}
+	if (debounceTimeoutId) {
+		clearTimeout(debounceTimeoutId);
+	}
+	debounceTimeoutId = setTimeout(async () => {
+		let result = await MWClient.prefixSearchAsync(val);
+		let resultMap = result.query?.prefixsearch.map(item => {
+			return {
+				label: item["title"]
+			};
+		});
+		articlePicker.items = resultMap ?? [];
+	}, DEBOUNCE_THRESHOLD);
+}
+articlePicker.onDidChangeValue(articlePickerDidChangeValueHandler);
+articlePicker.onDidAccept(async () => {
+	if (articlePicker.selectedItems.length === 0) {
+		console.log("Article Picker: ", articlePicker.value);
+		// TODO Prompt user to create new article.
+		articlePicker.hide();
+		let option = await vscode.window.showInformationMessage(`Would you like to create article ${articlePicker.value}?`, "Yes", "No");
+		console.log(option);
+		return;
+	}
+	let response = await vscode.window.withProgress({
+		"location" : vscode.ProgressLocation.Window,
+		"cancellable" : false,
+		"title": `Fetching ${articlePicker.selectedItems[0].label}`
+	}, async () => {
+		return MWClient.getRevisionAsync(articlePicker.selectedItems[0].label);
+	});
+	let pageIds = Object.getOwnPropertyNames(response.query.pages);
+	let revisions = pageIds.map(id => {
+		return {
+			"user": response.query.pages[id].revisions[0].user,
+			"content": response.query.pages[id].revisions[0].slots.main["*"]
+		};
+	});
+	for (let revision of revisions) {
+		let filename = path.join(SAVE_ROOT.fsPath, articlePicker.selectedItems[0].label + ".wiki");
+		let scheme = existsSync(filename) ? "file:" : "untitled:";
+		// let option = await vscode.window.showInformationMessage(`${findArticlePicker.selectedItems[0].label} already exists. Overwrite local file?`, Overwrite.YES, Overwrite.NO);
+		// if (option === Overwrite.YES) {
+
+		// }
+		let file = vscode.Uri.parse(scheme + filename);
+		let document = await vscode.workspace.openTextDocument(file);
+		// vscode.workspace.fs.stat(file).then(success => {
+		// 	console.log("stat: ", success);
+		// }, reject => {
+		// 	console.error("stat error: ", reject);
+		// });
+		let edit = new vscode.WorkspaceEdit();
+		if (document.getText() === "") {
+			edit.insert(file, new vscode.Position(0, 0), revision.content);
+		}
+		return vscode.workspace.applyEdit(edit).then(success => {
+			if (success) {
+				vscode.window.showTextDocument(document);
+			} else {
+				vscode.window.showInformationMessage("Failed to show document.");
+			}
+		});
+	}
+	articlePicker.hide();
+});
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension mediawiki-editor active.');
+	SAVE_ROOT = vscode.Uri.parse(context.globalStoragePath);
+	if (existsSync(SAVE_ROOT.fsPath)) { // Use this instead of /tmp because Windows not suprot /tmp
+		console.log("globalStoragePath: ", SAVE_ROOT.fsPath);
+	} else {
+		console.log("globalStoragePath does not exist. Creating it at: ", SAVE_ROOT.fsPath);
+		mkdirSync(SAVE_ROOT.fsPath);
+	}
 
-	let disposable = vscode.commands.registerCommand('mediawiki-editor.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from MediaWiki Editor!');
+	let findArticle = vscode.commands.registerCommand('mediawiki-editor.findArticle', async () => {
+		articlePicker.show();
 	});
 
-	context.subscriptions.push(disposable);
+	let loginCommand = vscode.commands.registerCommand('mediawiki-editor.login', async () => {
+		try {
+			let loginResponse = await MWClient.loginAsync(
+				await vscode.window.showInputBox({prompt: "Username", ignoreFocusOut: true}) || "",
+				await vscode.window.showInputBox({prompt: "Password", ignoreFocusOut: true, password: true}) || ""
+			);
+			if (loginResponse.clientlogin.status === 'PASS') {
+				vscode.window.showInformationMessage(`Successfully logged in as ${loginResponse.clientlogin.username}`);
+			} else {
+				vscode.window.showErrorMessage(`${loginResponse.clientlogin.status} (${loginResponse.clientlogin.messagecode}): ${loginResponse.clientlogin.message}`);
+			}
+		} catch (e) {
+			vscode.window.showErrorMessage(e);
+		}
+	});
+
+	let commitEditsCommand = vscode.commands.registerCommand('mediawiki-editor.commitEdits', async () => {
+		console.log("commitEditsCommand");
+		try {
+			let articlePath = await vscode.window.showQuickPick(vscode.workspace.textDocuments.map(docs => path.basename(docs.fileName)));
+			if (articlePath) {
+				let file = await vscode.workspace.fs.readFile(vscode.Uri.joinPath(SAVE_ROOT, articlePath));
+				let result = await MWClient.commitEditsAsync(path.basename(articlePath, ".wiki"), file.toString());
+				if (result.edit.result === 'Success') {
+					if (result.hasOwnProperty("nochange")) {
+						vscode.window.showInformationMessage(`Success. No change to ${result.edit.title}`);
+					} else {
+						vscode.window.showInformationMessage(`Success. Revision ${result.edit.newrevid} at ${result.edit.newtimestamp}`);
+					}
+				} else {
+					vscode.window.showErrorMessage(`Error, edit to ${result.edit.title} not successful.`);
+					console.log(result);
+				}
+			} else {
+				console.log("articlePath falsy: ", articlePath);
+			}
+		} catch (e) {
+			vscode.window.showErrorMessage(`${e.name}: ${e.message}`);
+		}
+	});
+
+	context.subscriptions.push(findArticle,loginCommand);
 }
 
 export function deactivate() {
---
+++ src/mwclient.ts
@@ -0,0 +1,259 @@
+import * as vscode from 'vscode';
+import * as FormData from 'form-data';
+import * as NodeFetch from 'node-fetch';
+const fetch = require('fetch-cookie')(NodeFetch);
+
+const WIKI_URL = vscode.workspace.getConfiguration("mediawiki-editor").get("wikiUrl");
+const AS_BOT = vscode.workspace.getConfiguration("mediawiki-editor").get("asBot");
+
+interface PSResult {
+    query?: {
+        prefixsearch: Array<PSResultItem>
+    }
+}
+
+interface PSResultItem {
+    ns: number
+    title: string
+    pageid: number
+}
+
+interface RVResult {
+    query: {
+        pages: any
+    }
+}
+
+interface TokenResult {
+	/***
+	 * @query Result body.
+	 * @tokens A map of the requested tokens.
+	 * @logintoken A token needed to use the login API.
+	 */
+	query: {
+		tokens: {
+			logintoken: string,
+			watchtoken?: string,
+			patroltoken?: string,
+			userrightstoken?: string,
+			createaccounttoken?: string,
+			deleteglobalaccounttoken?: string,
+			rollbacktoken?: string,
+			setglobalaccountstatustoken?: string,
+			csrftoken?: string
+		}
+	}
+}
+
+interface LoginResult {
+    /**
+     * @property {status} Either PASS or FAIL
+     * @property {message} Error message from the server.
+     * @property {messagecode} Accompanies {message}, describes type of error.
+     * @property {username} On PASS, will return name of username.
+     */
+	clientlogin: {
+		status: string,
+		message?: string,
+		messagecode?: string,
+		username?: string
+	}
+}
+
+interface EditResult {
+    /**
+     * @property {title} Title of article.
+     * @property {pageid} ID of article.
+     * @property {contentmodel} Format of wiki content in response body. Set to "wikitext".
+     * @property {result} "Success" or "Failure"
+     * @property {oldrevid} Old revision ID
+     * @property {newrevid} New revision ID of submitted edit.
+     * @property {newtimestamp} An ISO datetime string.
+     */
+    edit: {
+        result: string,
+        pageid: number,
+        title: string,
+        contentmodel: string,
+        oldrevid: number,
+        newrevid: number,
+        newtimestamp: string 
+    }
+}
+export enum TokenType {
+    CREATE_ACCOUNT = "createaccount",
+    CSRF = "csrf",
+    DELETE_GLOBAL_ACCOUNT = "deleteglobalaccount",
+    LOGIN = "login",
+    PATROL = "patrol",
+    ROLLBACK = "rollback",
+    SET_GLOBAL_ACCOUNT_STATUS = "setglobalaccountstatus",
+    USER_RIGHTS = "userrights",
+    WATCH = "watch"
+}
+
+export class MWClient {
+
+    /**
+     * @summary Search wiki articles by prefix. Does not search through
+     * article content other than the title.
+     */
+    public static async prefixSearchAsync(query: string): Promise<PSResult> {
+        return new Promise<PSResult>(async (resolve, reject) => {
+            let form = new FormData();
+            form.append('action', 'query');
+            form.append('list', 'prefixsearch');
+            form.append('pssearch', query);
+            form.append('psprofile', 'fuzzy');
+            form.append('format', 'json');
+    
+            let result = await fetch(`${WIKI_URL}/api.php`, {
+                method: 'POST',
+                body: form
+            })
+            .then(toJson);
+            resolve(result);
+        });
+    }
+
+    private static async getTokenAsync(token: TokenType): Promise<TokenResult> {
+        return new Promise(async (resolve, reject) => {
+            let form = new FormData();
+            form.append('action', 'query');
+            form.append('meta', 'tokens');
+            form.append('type', token);
+            form.append('format', 'json');
+            let response = await fetch(`${WIKI_URL}/api.php`, {
+                method: 'POST',
+                body: form
+            })
+            .then(statusCheck)
+            .then(logMessage(`FROM getTokenAsync: ${token}`))
+            .then(toJson);
+            resolve(response);
+        });
+    }
+
+    /**
+     * @summary Saves page content, creating a new revision.
+     * @param title 
+     * @param text 
+     */
+    public static async commitEditsAsync(title: string, text: string): Promise<EditResult> {
+        return new Promise(async (resolve, reject) => {
+            let token = await this.getTokenAsync(TokenType.CSRF);
+            if (token.query.tokens.csrftoken === '+\\') {
+                console.log("rejected");
+                return reject(new Error("Could not edit page. Token returned empty. Are you logged in?"));
+            }
+            let form = new FormData();
+            form.append('action', 'edit');
+            form.append('title', title);
+            form.append('notminor', 'true');
+            form.append('bot', String(AS_BOT));
+            // form.append('createonly', true);
+            form.append('format', 'json');
+            form.append('text', text);
+            form.append('token', token.query.tokens.csrftoken); // Token should always be last, or at least after the text parameter.
+    
+            let response = await fetch(`${WIKI_URL}/api.php`, {
+                method: 'POST',
+                body: form
+            })
+            .then(statusCheck)
+            .then(toJson)
+            .then(logMessage(`commitEdits with token ${token.query.tokens.csrftoken}`));
+        });
+    }
+
+    /**
+     * @summary Logs into wiki with provided credentials. No state is returned
+     * in the body, rather the library keeps a cookie session. This may change
+     * in the future to a more transparent/programmatic strategy.
+     */
+    public static async loginAsync(username: string, password: string): Promise<LoginResult> {
+        return new Promise(async (resolve, reject) => {
+            let tokenResult = await this.getTokenAsync(TokenType.LOGIN);
+            if (tokenResult.query.tokens.logintoken === "+\\") {
+                return reject(new Error("Empty login token."));
+            }
+            let form = new FormData();
+            form.append('action', 'clientlogin');
+            form.append('loginreturnurl', `${WIKI_URL}`);
+            form.append('logintoken', tokenResult.query.tokens.logintoken);
+            form.append('username', username);
+            form.append('password', password);
+            form.append('format', 'json');
+    
+            let response = await fetch(`${WIKI_URL}/api.php`, {
+                method: 'POST',
+                body: form
+            })
+            .then(statusCheck)
+            .then(toJson)
+            .then(logIt);
+            resolve(response);
+        });
+    }
+
+    /**
+     * Get a revision by ID.
+     * @param {id} An ID of a specific revision.
+     */
+    public static async getRevisionAsync(id: number): Promise<RVResult>;
+    /**
+     * Get the latest revision of an article by its title.
+     * @param {title} The title of an article.
+     */
+    public static async getRevisionAsync(title: string): Promise<RVResult>;
+    public static async getRevisionAsync(id: string | number): Promise<RVResult> {
+        return new Promise<RVResult>(async (resolve, reject) => {
+            let form = new FormData();
+            form.append("action", "query");
+            form.append("prop", "revisions");
+            form.append("format", "json");
+            form.append("rvprop", "content|user|timestamp");
+            form.append("rvslots", "main");
+            form.append("curtimestamp", "true");
+            form.append("errorformat", "wikitext");
+            if (typeof id === "string") {
+                console.log("id is STRING");
+                form.append('titles', id);
+            } else if (typeof id === "number") {
+                form.append('revids', String(id));
+            }
+
+            let response = await fetch(`${WIKI_URL}/api.php`, {
+                method: 'POST',
+                body: form
+            })
+            .then(toJson)
+            .then(logIt);
+            resolve(response);
+        });
+    }
+}
+
+function logIt(result: any) {
+    console.log(result);
+    return Promise.resolve(result);
+}
+function toJson(result: NodeFetch.Response) {
+    return Promise.resolve(result.json());
+}
+function statusCheck(result: NodeFetch.Response) {
+    if (result.ok) {
+        console.log(`STATUS OK: ${result.status}`);
+        return Promise.resolve(result);
+    } else {
+        console.log(`STATUS: ${result.status}`);
+        return Promise.reject(result);
+    }
+}
+
+function logMessage(message: string) {
+    return (result: any) => {
+        console.log(`${message}`);
+        return Promise.resolve(result);
+    };
+}
